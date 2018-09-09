8d5b3303caa6251c1a7f9b615f44745752b70590
Samuel Berman <11687897+SamuelBerman@users.noreply.github.com>
2018-09-09 13:47:15 -0700

    added constaints

--- js/script.js
+++ js/script.js
@@ -37,23 +37,32 @@ var Engine = Matter.Engine,
 // create an engine
 var engine = Engine.create();
 
-var pogoStick = Bodies.rectangle(window.innerWidth/2, 270, 20, 45, {density:0.002, friction:1});
+var pogoStick = Bodies.rectangle(window.innerWidth/2, 400, 20, 80, {density:0.002, friction:1});
 var pogoBody = Bodies.rectangle(window.innerWidth/2, 225, 30, 90, {density:0.002, friction:1});
 
 pogoBody.col = '#FF0000';
 pogoStick.col = '#FF6600';
+pogoBody.collisionFilter.group = -1;
+pogoStick.collisionFilter.group = -1;
 
-var options = {
-    bodyA: pogoStick.body,
-    bodyB: pogoBody.body,
-    stiffness: 0.005,
-    damping: 1,
-    length: 1
-}
+var spring = Constraint.create ({
+    pointA: {x:0,y:-80},
+    bodyA: pogoStick,
+    bodyB: pogoBody,
+    stiffness: 0.1,
+    length: 0
+});
 
-var spring = Constraint.create(options);
+var spring2 = Constraint.create ({
+    pointB: {x:0,y:40},
+    bodyA: pogoStick,
+    bodyB: pogoBody,
+    stiffness: 0.1,
+    length: 0
+});
 
-World.add(engine.world, [pogoBody, pogoStick, spring]);
+
+World.add(engine.world, [pogoBody, pogoStick, spring, spring2]);
 
 var offset = 1;
 var wallSize = 20;
