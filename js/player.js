00681842cd36d679891452454deda12155a039e7
Samuel Berman <11687897+SamuelBerman@users.noreply.github.com>
2019-06-05 16:44:41 -0700

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,8 +1,8 @@
 var pogoStick = Bodies.rectangle(window.innerWidth/2, 600, 20, 80, {density:0.002, friction:2});
 var pogoBody = Bodies.rectangle(window.innerWidth/2, 600, 30, 90, {density:0.004, friction:1});
 
-pogoBody.col = '#FF0000';
-pogoStick.col = '#FF6600';
+pogoBody.col = '#a31919';
+pogoStick.col = '#d19019';
 pogoBody.collisionFilter.group = -1;
 pogoStick.collisionFilter.group = -1;
 
